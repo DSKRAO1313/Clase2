c53339e593cd9ec7ab8e8d18541a05947fcc3ed1
alumnos.republica <alumnos.republica@REP-AL-ECI05-23>
2025-08-29 20:19:16 -0400

    Agregar archivos de proyecto.

---
+++ Clase2/Form1.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        numero1 = New TextBox()
+        numero2 = New TextBox()
+        btcalcular = New Button()
+        num1 = New Label()
+        num2 = New Label()
+        Cm = New ComboBox()
+        SuspendLayout()
+        ' 
+        ' numero1
+        ' 
+        numero1.Location = New Point(60, 74)
+        numero1.Name = "numero1"
+        numero1.Size = New Size(100, 23)
+        numero1.TabIndex = 0
+        ' 
+        ' numero2
+        ' 
+        numero2.Location = New Point(60, 140)
+        numero2.Name = "numero2"
+        numero2.Size = New Size(100, 23)
+        numero2.TabIndex = 1
+        ' 
+        ' btcalcular
+        ' 
+        btcalcular.Location = New Point(243, 100)
+        btcalcular.Name = "btcalcular"
+        btcalcular.Size = New Size(75, 23)
+        btcalcular.TabIndex = 2
+        btcalcular.Text = "Button1"
+        btcalcular.UseVisualStyleBackColor = True
+        ' 
+        ' num1
+        ' 
+        num1.AutoSize = True
+        num1.Location = New Point(62, 51)
+        num1.Name = "num1"
+        num1.Size = New Size(87, 15)
+        num1.TabIndex = 3
+        num1.Text = "primer numero"
+        ' 
+        ' num2
+        ' 
+        num2.AutoSize = True
+        num2.Location = New Point(62, 116)
+        num2.Name = "num2"
+        num2.Size = New Size(98, 15)
+        num2.TabIndex = 4
+        num2.Text = "segundo numero"
+        ' 
+        ' Cm
+        ' 
+        Cm.FormattingEnabled = True
+        Cm.Location = New Point(60, 216)
+        Cm.Name = "Cm"
+        Cm.Size = New Size(121, 23)
+        Cm.TabIndex = 5
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(344, 293)
+        Controls.Add(Cm)
+        Controls.Add(num2)
+        Controls.Add(num1)
+        Controls.Add(btcalcular)
+        Controls.Add(numero2)
+        Controls.Add(numero1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents numero1 As TextBox
+    Friend WithEvents numero2 As TextBox
+    Friend WithEvents btcalcular As Button
+    Friend WithEvents num1 As Label
+    Friend WithEvents num2 As Label
+    Friend WithEvents Cm As ComboBox
+
+End Class
---
+++ Clase2/Form1.vb
@@ -0,0 +1,47 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Cm.Items.Add("suma")
+        Cm.Items.Add("resta")
+        Cm.Items.Add("multiplicacion")
+        Cm.Items.Add("division")
+    End Sub
+
+    Private Sub btcalcular_Click(sender As Object, e As EventArgs) Handles btcalcular.Click
+        Try
+            Dim numero1 As Double
+            Dim numero2 As Double
+            Dim resultado As Double
+
+            Select Case Cm.SelectedItem.ToString()
+                Case "+"
+                    resultado = numero1 + numero2
+                Case "-"
+                    resultado = numero1 - numero2
+                Case "*"
+                    resultado = numero1 * numero2
+                Case "/"
+                    If numero2 <> 0 Then
+                        resultado = numero1 / numero2
+                    Else
+                        MessageBox.Show("No se puede dividir entre 0")
+                        Exit Sub
+                    End If
+                Case Else
+                    MessageBox.Show("Seleccione una operación válida")
+                    Exit Sub
+            End Select
+
+
+            MessageBox.Show("El resultado es: " & resultado.ToString())
+
+        Catch ex As Exception
+            MessageBox.Show("Por favor ingrese números válidos")
+        End Try
+    End Sub
+
+    Private Sub numero1_TextChanged(sender As Object, e As EventArgs) Handles numero1.TextChanged
+
+    End Sub
+End Class
